8b4814aab6f410536b23a95e3ff127abd3620b79
Raj kumar mahto <rk2519962@gmail.com>
2023-01-08 14:53:12 +0530

    update

--- cointab/backend/Config/db.js
+++ cointab/backend/Config/db.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose")
+require("dotenv").config();
+mongoose.set('strictQuery', true)
 
-const connection = mongoose.connect("mongodb+srv://rajkumarmahto:rajkumarmahto@cluster0.nkn5o15.mongodb.net/?retryWrites=true&w=majority")
+const connection = mongoose.connect(process.env.MONGOURL)
 
 module.exports={connection}
