b7f1cfb166054b0f66d395b31dd05bb74560b579
Raj kumar mahto <rk2519962@gmail.com>
2023-01-08 15:02:16 +0530

    update

--- cointab/backend/index.js
+++ cointab/backend/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const { userRoute } = require("./Routes/user.routes");
 require("dotenv").config();
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 8000 ;
 
 const app = express();
 
